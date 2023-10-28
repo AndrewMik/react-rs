241d7c199b9cad6896921c5917ae7a1754f36809
Andrew Mikitchuk <andrew.mikitchuk@gmail.com>
2023-10-28 14:57:57 +0400

    feat: implement basic layout

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+import './style.css';
+
+export default class Search extends Component {
+  render() {
+    return <section className='search'>
+      <input></input>
+      <button>Search</button>
+    </section>;
+  }
+}
---
+++ src/components/SearchResults/SearchResults.tsx
@@ -0,0 +1,8 @@
+import { Component } from 'react';
+import './style.css';
+
+export default class SearchResults extends Component {
+  render() {
+    return <section className='search-results'></section>;
+  }
+}
