d065fcdbedab7b8bd6ee7c1a2541e8285e390343
Andrew Mikitchuk <andrew.mikitchuk@gmail.com>
2023-10-29 17:57:48 +0400

    refactor: move state logic to Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,33 @@
 import { Component } from 'react';
 import './style.css';
 import SearchResults from '../SearchResults/SearchResults';
+import { getCharacters } from '../../api/getCharacters';
+import { Character } from '../Characters/Characters';
+
+type SearchState = {
+  searchResults: Character[];
+  loading: boolean;
+};
 
 export default class Search extends Component {
+  state: SearchState = {
+    searchResults: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    this.loadInitialData();
+  }
+
+  loadInitialData = async () => {
+    try {
+      const results: Character[] = await getCharacters();
+      this.setState({ searchResults: results });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
     return (
       <>
@@ -10,7 +35,7 @@ export default class Search extends Component {
           <input></input>
           <button>Search</button>
         </section>
-        <SearchResults />
+        <SearchResults searchResults={this.state.searchResults}/>
       </>
     );
   }
--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,40 +1,21 @@
 import { Component } from 'react';
 import './style.css';
-import { getCharacters } from '../../api/getCharacters';
+
 import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 import Characters, { Character } from '../Characters/Characters';
 
-type SearchResultsState = {
+type SearchResultsProps = {
   searchResults: Character[];
-  loading: boolean;
 };
 
-export default class SearchResults extends Component {
-  state: SearchResultsState = {
-    searchResults: [],
-    loading: true,
-  };
-
-  componentDidMount() {
-    this.loadInitialData();
-  }
-
-  loadInitialData = async () => {
-    try {
-      const results: Character[] = await getCharacters();
-      this.setState({ searchResults: results });
-    } finally {
-      this.setState({ loading: false });
-    }
-  };
-
+export default class SearchResults extends Component<SearchResultsProps> {
   render() {
-    const { searchResults } = this.state;
+    const { searchResults } = this.props;
 
     return (
       <section className="search-section">
         <h2 className="search-results">Search Results</h2>
-        {!this.state.loading && searchResults.length > 0 ? (
+        {searchResults.length > 0 ? (
           <Characters characters={searchResults} />
         ) : (
           <LoadingSpinner />
