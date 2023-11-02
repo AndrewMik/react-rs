46ffca5217983ed7bf98296b8908308829f29247
Andrew Mikitchuk <andrew.mikitchuk@gmail.com>
2023-11-02 21:40:39 +0400

    refactor: change Spinner to functional component

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,12 +1,11 @@
-import { Component } from 'react';
 import './style.css';
 
-export default class LoadingSpinner extends Component {
-  render() {
-    return (
-      <div className="spinner-container">
-        <div className="loading-spinner"></div>
-      </div>
-    );
-  }
-}
+const LoadingSpinner: React.FC = () => {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
