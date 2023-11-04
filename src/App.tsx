8775439578f75c611b1917c163851b2300c686bd
Andrew Mikitchuk <andrew.mikitchuk@gmail.com>
2023-11-04 12:12:30 +0400

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import Search from './components/Search/Search';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import { Route as RoutePath } from './routes';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path={RoutePath.Home} element={<Search />} />)
+);
 const App: React.FC = () => {
   return (
     <ErrorBoundary>
-      <Search />
+      <RouterProvider router={router}></RouterProvider>
     </ErrorBoundary>
   );
 };
