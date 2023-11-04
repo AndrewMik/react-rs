0df80ded2f101e7da4b2cea85f9cce497e648b74
Andrew Mikitchuk <andrew.mikitchuk@gmail.com>
2023-11-04 09:23:42 +0400

    refactor: change App to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { Component } from 'react';
 import Search from './components/Search/Search';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
 
-class App extends Component {
-  render() {
-    return (
-      <ErrorBoundary>
-        <Search />
-      </ErrorBoundary>
-    );
-  }
-}
+const App: React.FC = () => {
+  return (
+    <ErrorBoundary>
+      <Search />
+    </ErrorBoundary>
+  );
+};
 
 export default App;
